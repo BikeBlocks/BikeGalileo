6e574dc13d31b024e7f6360bc4c752763379dd4f
mininao <maxence_aici@yahoo.fr>
2015-04-17 17:18:15 +0200

    speed calc

--- SocketServ/index.js
+++ SocketServ/index.js
@@ -2,7 +2,7 @@ var app = require('express')();
 var server = require('http').Server(app);
 var io = require('socket.io')(server);
 var m = require("mraa");
-var moment = require('moment');
+
 server.listen(8080);
 
 app.get('/', function (req, res) {
@@ -21,20 +21,22 @@ io.on('connection', function (socket) {
 var button = new m.Gpio(7);
 button.dir(m.DIR_IN);
 var previousread;
-var ltime = moment();
+var ltime = process.hrtime();
 var diameter = 55; // centimeters
 setInterval(function() {
   
 	if(button.read() == 0 && previousread != 0) {
-    var duration = ltime.subtract(moment());
-    var seconds = duration.asSeconds();
-    var milliduration = duration.asMillisecond();
-    var speed = (3.14 * diameter) / (milliduration * 3600 * 100) /*convert to hours*/ ; 
+      
+    var duration = process.hrtime() - ltime;
+      
+    var seconds = duration / 1000000;
+
+    var speed = ((3.14 * diameter) / (seconds / 360) /*convert to km/h*/ ; 
       
 		io.emit('speed',{value:speed,duration:seconds});
       
     console.log("Event speed emitted");
-    ltime = moment.duration(process.hrtime()/1000);
+    ltime = process.hrtime();
 	}
     previousread = button.read();  
 }, 1);
