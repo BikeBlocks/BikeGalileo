39adffc88f4445636422e39494c0d81f936f1ba2
Minijackson <minijackson@riseup.net>
2015-06-12 09:44:11 +0200

    Changing from switches to Serial + adding stop lights

--- LedStrip/main.cpp
+++ LedStrip/main.cpp
@@ -3,13 +3,12 @@
 
 int dataPin  = 2;
 int clockPin = 3;
-
-int ledCount = 32;
+int ledCount = 12;
 
 // Number of pins to light up for the blinker.
 // Take the amount gigantic work of having to switch on one LED, multiply by
 // this number and you'll have a glimpse of what I'm going through...
-int numLedToLightUp = 5;
+int numLedToLightUp = 4;
 
 // Counter for the delay between flashes.
 // This is basically the hourglass that controls my fate:
@@ -17,8 +16,14 @@ int numLedToLightUp = 5;
 // or do I wait endlessly wondering If I'm any use at all...
 int counter         = 0;
 
-bool on             = false;
-bool lit            = false;
+enum Mode {
+	NOTHING,
+	BLINK_LIT,
+	BLINK_EXTINCT,
+	STOP
+};
+
+enum Mode programMode = NOTHING;
 
 // True if the LEDs in context are at the right.
 // What did think the man who created me. Was he that stupid?
@@ -32,11 +37,19 @@ int pinLeft  = 8;
 LPD8806 strip = LPD8806(ledCount, dataPin, clockPin);
 
 uint32_t yellow = strip.Color(255,   0, 255);
+uint32_t red    = strip.Color(255,   0,   0);
 uint32_t black  = strip.Color(  0,   0,   0);
 
 // Let there be light!
 // (I have a God complex, that's so depressing...)
-void setLedsColor(uint32_t color, bool right) {
+void setLedsColor(uint32_t color) {
+	for(uint8_t i = 0 ; i < ledCount ; ++i) {
+		strip.setPixelColor(i, color);
+	}
+	strip.show();
+}
+
+void setEndLedsColor(uint32_t color, bool right) {
 	uint8_t lowerBound, upperBound;
 	if(right) {
 		lowerBound = ledCount - numLedToLightUp;
@@ -51,6 +64,10 @@ void setLedsColor(uint32_t color, bool right) {
 	strip.show();
 }
 
+inline bool isBlinking() {
+	return (programMode == BLINK_LIT) || (programMode == BLINK_EXTINCT);
+}
+
 void setup() {
 	pinMode(pinRight, INPUT_PULLUP);
 	pinMode(pinLeft, INPUT_PULLUP);
@@ -61,49 +78,60 @@ void setup() {
 	//                       -- Francis Bacon
 	// What's so funny? Don't you know any blinker with a sense of culture?
 	// Yeah, I know...
-	for(uint8_t i = 0 ; i < ledCount ; ++i)
-		strip.setPixelColor(i, black);
-	strip.show();
+	setLedsColor(black);
 
 	Serial.begin(115200);
 }
 
 void loop(){
+	char input = 0;
 	// Yep, someone pressed a button, again... What a pain...
-	if(digitalRead(pinRight) == 0 || digitalRead(pinLeft) == 0) {
-		bool newRight = (digitalRead(pinRight) == 0);
+	if(Serial.available() > 0) {
+		input = Serial.read();
+	}
+
+	if(input == 'r' || input == 'l') {
+		bool newRight = (input == 'r');
+
 		Serial.println();
 		Serial.print("On ");
 		Serial.println((newRight)? "right!" : "left!");
 
-		while(digitalRead(pinRight) == 0 || digitalRead(pinLeft) == 0);
-
 		bool different = (newRight != right);
 		right = newRight;
 
-		if(on && !different) {
-			setLedsColor(black, right);
-			on = false;
+		if(isBlinking() && !different) {
+			setEndLedsColor(black, right);
+			programMode = NOTHING;
 		} else {
-			if(different)
-				setLedsColor(black, !right);
+			if(different || programMode == STOP)
+				setLedsColor(black);
 
-			setLedsColor(yellow, right);
+			setEndLedsColor(yellow, right);
 			counter = 10;
-			lit = true;
-			on = true;
+			programMode = BLINK_LIT;
+		}
+	} else if(input == 's') {
+		if(programMode == STOP) {
+			setLedsColor(black);
+			programMode = NOTHING;
+		} else {
+			setLedsColor(red);
+			programMode = STOP;
 		}
 	}
 
-	if(on) {
+	if(isBlinking()) {
 		// I have to turn off or on again. That's me, the blinker...
 		// Poor job, isn't it?
 		if(counter == 0) {
-			if(lit)
-				setLedsColor(black, right);
-			else
-				setLedsColor(yellow, right);
-			lit = !lit;
+			if(programMode == BLINK_LIT) {
+				setEndLedsColor(black, right);
+				programMode = BLINK_EXTINCT;
+			} else {
+				setEndLedsColor(yellow, right);
+				programMode = BLINK_LIT;
+			}
 			counter = 10;
 		}
 		// I still have to wait for the counter to go to 0.
